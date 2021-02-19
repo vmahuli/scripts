ef4f0beebb43900087c75fdfdb96e57bfa05c053
Vinay Mahuli <vinay-m@live.com>
2021-02-20 01:32:08 +0530

    Update find_dupes.sh

--- dup_files/find_dupes.sh
+++ dup_files/find_dupes.sh
@@ -3,13 +3,13 @@
 # to find the list of duplicate files in a directory tree
 
 #get list of files
-find . -type f > all_files
+find . -type f > $PWD/all_files
 
 #get md5 sum of all files into a file
-for file in `cat all_files`
+for file in `cat $PWD/all_files`
 do
   md5value=$(md5sum ${file})
-  echo "${md5value}" >> files_with_md5
+  echo "${md5value}" >> $PWD/files_with_md5
 done
 
 # grep for md5sum in files_with_md5 and get those
@@ -17,12 +17,12 @@ done
 echo "Duplicate files..."
 while read line
 do
-   md5sumoffile=$(echo ${line} | tr -s ' ' | cut -d ' ' -f1)   
+   md5sumoffile=$(echo ${line} | tr -s ' ' | cut -d ' ' -f1)
    file_name=$(echo ${line} | tr -s ' ' | cut -d ' ' -f2)
-   file_occurance_count=$(grep $md5sumoffile files_with_md5 | wc -l)
+   file_occurance_count=$(grep $md5sumoffile $PWD/files_with_md5 | wc -l)
    if [ ${file_occurance_count} -gt 1 ]; then
        echo $file_name occurs ${file_occurance_count} times
    fi
-done < files_with_md5
+done < $PWD/files_with_md5
 
-rm -f all_files files_with_md5
+rm -f $PWD/all_files $PWD/files_with_md5
